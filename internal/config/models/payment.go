d9b3588191f946a2d46f5b074b6f557b3537d1e8
Sterling95 <denxyzaev234@yandex.ru>
2025-11-20 16:40:42 +0300

    добавил валидированыее статусы

--- internal/config/models/payment.go
+++ internal/config/models/payment.go
@@ -1,19 +1,45 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type PaymentStatus string
+
+const (
+	PaymentStatusPending PaymentStatus = "pending"
+	PaymentStatusSuccess PaymentStatus = "success"
+	PaymentStatusFailed  PaymentStatus = "failed"
+)
+
+type PaymentMethod string
+
+const (
+	PaymentMethodCard         PaymentMethod = "card"
+	PaymentMethodCash         PaymentMethod = "cash"
+	PaymentMethodBankTransfer PaymentMethod = "bank_transfer"
+)
 
 type Payment struct {
-	Amount int 		`json:"amount"`
-	Status string 	`json:"status"`
-	Method string	`json:"method"`
-	Paid_at time.Time `json:"paid_at"`
+	gorm.Model
+	Amount  int           `json:"amount"`
+	Status  PaymentStatus `json:"-"`
+	Method  PaymentMethod `json:"-"`
+	Paid_at time.Time     `json:"paid_at"`
 }
 
-type CreatePayment struct {
-	Amount int 		`json:"amount" binding:"required"`
-	Status string 	`json:"status" binding:"required"` 
-	Method string	`json:"method" binding:"required"`
+type CreatePaymentRequest struct {
+	Amount  int       `json:"amount" binding:"required"`
+	Status  PaymentStatus    `json:"status" binding:"required"`
+	Method  PaymentMethod    `json:"method" binding:"required"`
 	Paid_at time.Time `json:"paid_at" binding:"required"`
 }
 
-
+type UpdatePaymentRequest struct {
+	Amount  *int       `json:"amount"`
+	Status  *PaymentStatus    `json:"status"`
+	Method  *PaymentMethod    `json:"method"`
+	Paid_at *time.Time `json:"paid_at"`
+}
