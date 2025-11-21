6505fb29acff460b18c8ae74c2bb7c37ad86f03e
amir-hayd <amirhaidaev@icloud.com>
2025-11-21 14:51:00 +0300

    Update medicines.go

--- internal/config/models/medicines.go
+++ internal/config/models/medicines.go
@@ -7,36 +7,36 @@ type Medicine struct {
 	Name                  string  `json:"name"`
 	Description           string  `json:"description"`
 	Price                 int     `json:"price"`
-	In_stock              bool    `json:"in_stock"`
-	Stock_quantity        uint    `json:"stock_quantity"`
-	Category_id           string  `json:"category_id"`
-	Subcategory_id        string  `json:"subcategory_id"`
+	InStock              bool    `json:"in_stock"`
+	StockQuantity        uint    `json:"stock_quantity"`
+	CategoryId           string  `json:"category_id"`
+	SubcategoryId        string  `json:"subcategory_id"`
 	Manufacturer          string  `json:"manufacturer"`
-	Prescription_required bool    `json:"prescription_required"`
-	Avg_rating            float64 `gorm:"-" json:"avg_raiting"`
+	PrescriptionRequired bool    `json:"prescription_required"`
+	AvgRating            float64 `gorm:"-" json:"avg_raiting"`
 }
 
 type CreateMedicineRequest struct {
 	Name                  string  `json:"name"`
 	Description           string  `json:"description"`
 	Price                 int     `json:"price"`
-	In_stock              bool    `json:"in_stock"`
-	Stock_quantity        uint    `json:"stock_quantity"`
-	Category_id           string  `json:"category_id"`
-	Subcategory_id        string  `json:"subcategory_id"`
+	InStock              bool    `json:"in_stock"`
+	StockQuantity        uint    `json:"stock_quantity"`
+	CategoryId           string  `json:"category_id"`
+	SubcategoryId        string  `json:"subcategory_id"`
 	Manufacturer          string  `json:"manufacturer"`
-	Prescription_required bool    `json:"prescription_required"`
-	Avg_rating            float64 `gorm:"-" json:"avg_raiting"`
+	PrescriptionRequired bool    `json:"prescription_required"`
+	AvgRating            float64 `gorm:"-" json:"avg_raiting"`
 }
 type UpdateMedicineRequest struct {
 	Name                  *string  `json:"name"`
 	Description           *string  `json:"description"`
 	Price                 *int     `json:"price"`
-	In_stock              *bool    `json:"in_stock"`
-	Stock_quantity        *uint    `json:"stock_quantity"`
-	Category_id           *string  `json:"category_id"`
-	Subcategory_id        *string  `json:"subcategory_id"`
+	InStock              *bool    `json:"in_stock"`
+	StockQuantity        *uint    `json:"stock_quantity"`
+	CategoryId           *string  `json:"category_id"`
+	SubcategoryId        *string  `json:"subcategory_id"`
 	Manufacturer          *string  `json:"manufacturer"`
-	Prescription_required *bool    `json:"prescription_required"`
-	Avg_rating            *float64 `gorm:"-" json:"avg_raiting"`
+	PrescriptionRequired *bool    `json:"prescription_required"`
+	AvgRating            *float64 `gorm:"-" json:"avg_raiting"`
 }
