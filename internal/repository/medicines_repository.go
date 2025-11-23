5efb26f55e91f7e27c830c7af18d78d7c760496b
amir-hayd <amirhaidaev@icloud.com>
2025-11-23 18:11:15 +0300

    сделал репозиторий

---
+++ internal/repository/medicines_repository.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"github.com/kuduzow/team-5-pharmacy/internal/models"
+	"gorm.io/gorm"
+)
+
+type MedicineRepository interface {
+	Create(medicine *models.Medicine) error
+	 UpdateCategoryRequest(medicine *models.Medicine) error
+	DeleteMedicines(medicine *models.Medicine) error
+	GetMedecinesRepository(medicine *models.Medicine)error
+}
+
+type gormMedicineRepository struct {
+	db *gorm.DB
+}
+
+
+func NewMedicinesRepository(db *gorm.DB) MedicineRepository {
+	return &gormMedicineRepository{db: db}
+}
+
+
+
+func (r *gormMedicineRepository) Create(medicine *models.Medicine) error {
+	if medicine == nil {
+
+		return nil
+	}
+	return r.db.Create(medicine).Error
+}
+func (r *gormMedicineRepository) UpdateCategoryRequest(medicine *models.Medicine) error {
+	if medicine == nil {
+
+		return nil
+	}
+	return r.db.Save(medicine).Error
+}
+func (r *gormMedicineRepository) DeleteMedicines(medicine *models.Medicine) error {
+	if medicine == nil {
+
+		return nil
+	}
+	return r.db.Delete(medicine).Error
+}
+func (r *gormMedicineRepository) GetMedecinesRepository(medicine *models.Medicine) error {
+	if medicine == nil {
+
+		return nil
+	}
+	return r.db.First(medicine).Error
+}
+func (r *gormMedicineRepository) Exists(id uint) (bool, error) {
+	var count int64 // сюда сохраним количество найденных записей
+
+	// SELECT COUNT(*) FROM groups WHERE id = ?
+	err := r.db.
+		Model(&models.Medicine{}).
+		Where("id = ?", id).
+		Count(&count).
+		Error
+	if err != nil {
+		return false, err
+	}
+
+	return count > 0, nil
+}
