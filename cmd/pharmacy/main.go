66495841eabff38ad986c2d24532e2c7de3dd3c1
kuduzow <ammmya@gmail.com>
2025-11-25 11:34:23 +0300

    Add review model, repository, and integrate into main application
    - Introduced Review model with updated fields and foreign key relationships for User and Medicine.
    - Created Review repository with a constructor for instantiation.
    - Updated main.go to initialize Review repository, service, and transport handler, integrating them into the application routing.

--- cmd/pharmacy/main.go
+++ cmd/pharmacy/main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kuduzow/team-5-pharmacy/internal/config"
 	"github.com/kuduzow/team-5-pharmacy/internal/models"
+	"github.com/kuduzow/team-5-pharmacy/internal/repository"
+	"github.com/kuduzow/team-5-pharmacy/internal/services"
+	"github.com/kuduzow/team-5-pharmacy/internal/transport"
 )
 
 func main() {
@@ -16,19 +19,36 @@ func main() {
 		&models.Subcategory{},
 		&models.Medicine{},
 		&models.Payment{},
+		&models.Review{},
 	); err != nil {
 		log.Fatalf("не удалось выполнить миграции: %v", err)
 	}
 
+	// Инициализация репозиториев
+	categoryRepo := repository.NewCategoryRepository(db)
+	subcategoryRepo := repository.NewSubcategoryRepository(db)
+	paymentRepo := repository.NewPaymentRepository(db)
+	reviewRepo := repository.NewReviewRepository(db)
+
+	// Инициализация сервисов
+	categoryService := services.NewCategoryService(categoryRepo)
+	subcategoryService := services.NewSubcategoryService(subcategoryRepo)
+	paymentService := services.NewPaymentService(paymentRepo)
+	reviewService := services.NewReviewService(reviewRepo)
+
+	// Инициализация хэндлеров
+	categoryHandler := transport.NewCategoryHandler(categoryService)
+	subcategoryHandler := transport.NewSubcategoryHandler(subcategoryService)
+	paymentHandler := transport.NewPaymentHandler(paymentService)
+	reviewHandler := transport.NewReviewHandler(reviewService)
+
 	router := gin.Default()
-	// reviewHandler := handlers.NewReviewHandler(db)
 
-	// router.POST("/reviews", reviewHandler.Create)
-	// router.GET("/medicines/:id/reviews", reviewHandler.GetByMedicineID)
-	// router.PATCH("/reviews/:id", reviewHandler.Update)
-	// router.DELETE("/reviews/:id", reviewHandler.Delete)
-
-	// регистрация роутов
+	// Регистрация роутов
+	categoryHandler.RegisterRoutes(router)
+	subcategoryHandler.RegisterRoutes(router)
+	paymentHandler.RegisterRoutes(router)
+	reviewHandler.RegisterRoutes(router)
 
 	if err := router.Run(); err != nil {
 		log.Fatalf("не удалось запустить HTTP-сервер: %v", err)
