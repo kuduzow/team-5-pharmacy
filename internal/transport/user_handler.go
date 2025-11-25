96c2b7762ff75872f8064ce352f9020a0638b455
DjMariarty <->
2025-11-25 14:36:40 +0300

    Создал транспорт для своей сущности и задал его логику

---
+++ internal/transport/user_handler.go
@@ -0,0 +1,117 @@
+package transport
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kuduzow/team-5-pharmacy/internal/models"
+	"github.com/kuduzow/team-5-pharmacy/internal/services"
+)
+
+type UserHandler struct {
+	service services.UserService
+}
+
+func NewUserHandler(service services.UserService) *UserHandler {
+	return &UserHandler{service: service}
+}
+
+func (h *UserHandler) RegisterRoutes(r *gin.Engine) {
+
+	users := r.Group("/users")
+
+	{
+		users.POST("", h.Create)
+
+		users.GET("/:id", h.Get)
+
+		users.PATCH("/:id", h.Update)
+
+		users.DELETE("/:id", h.Delete)
+	}
+}
+
+func (h *UserHandler) Create(c *gin.Context) {
+
+	var req models.CreateUserRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := h.service.CreateUser(req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+}
+
+func (h *UserHandler) Get(c *gin.Context) {
+	idstr := c.Param("id")
+
+	id, err := strconv.ParseInt(idstr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "некорректный идентификатор"})
+		return
+	}
+
+	user, err := h.service.GetUserByID(uint(id))
+	if err != nil {
+		if errors.Is(err, services.ErrUserNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+
+}
+
+func (h *UserHandler) Update(c *gin.Context) {
+	idStr := c.Param("id")
+
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "некорректный идентификатор"})
+		return
+	}
+
+	var req models.UpdateUserRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err1 := h.service.UpdateUser(uint(id), req)
+	if err1 != nil {
+		if errors.Is(err, services.ErrUserNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *UserHandler) Delete(c *gin.Context) {
+	idStr := c.Param("id")
+
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "некорректный идентификатор"})
+		return
+	}
+	if err := h.service.DeleteUser(uint(id)); err != nil {
+		if errors.Is(err, services.ErrUserNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+	}
+	c.Status(http.StatusOK)
+}
