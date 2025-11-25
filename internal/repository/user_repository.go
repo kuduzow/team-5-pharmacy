1237d11cfa871f07ae368a6ea6a40f31ab3a66a7
DjMariarty <->
2025-11-25 10:41:48 +0300

    user service

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"github.com/kuduzow/team-5-pharmacy/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *models.User) error
+
+	GetByID(id uint) (*models.User, error)
+
+	Update(user *models.User) error
+
+	Delete(id uint) error
+}
+
+type gormUserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &gormUserRepository{db: db}
+}
+
+func (r *gormUserRepository) Create(user *models.User) error {
+	if user != nil {
+		return nil
+	}
+	return r.db.Create(user).Error
+}
+
+func (r *gormUserRepository) GetByID(id uint) (*models.User, error) {
+	var user models.User
+	if err := r.db.First(&user, id).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *gormUserRepository) Update(user *models.User) error {
+	if user == nil {
+
+		return nil
+	}
+	return r.db.Save(user).Error
+}
+
+func (r *gormUserRepository) Delete(id uint) error {
+	return r.db.Delete(&models.User{}, id).Error
+}
