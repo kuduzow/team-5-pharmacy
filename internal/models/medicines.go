f149c2df823ebb68b9e6d0f46654dc5fe27631a4
DjMariarty <->
2025-11-26 12:54:52 +0300

    оадлаоад

--- internal/models/medicines.go
+++ internal/models/medicines.go
@@ -4,39 +4,39 @@ import "gorm.io/gorm"
 
 type Medicine struct {
 	gorm.Model
-	Name                  string  `json:"name"`
-	Description           string  `json:"description"`
-	Price                 int     `json:"price"`
-	InStock              bool    `json:"instock"`
-	StockQuantity        uint    `json:"stockquantity"`
-	CategoryId           string  `json:"categoryid"`
-	SubcategoryId        string  `json:"subcategoryid"`
-	Manufacturer          string  `json:"manufacturer"`
-	PrescriptionRequired bool    `json:"prescriptionrequired"`
-	AvgRating            float64 `gorm:"-" json:"avgraiting"`
+	Name                 string  `json:"name"`
+	Description          string  `json:"description"`
+	Price                int     `json:"price"`
+	InStock              bool    `json:"in_stock"`
+	StockQuantity        uint    `json:"stock_quantity"`
+	CategoryId           uint    `json:"category_id"`
+	SubcategoryId        uint    `json:"subcategory_id"`
+	Manufacturer         string  `json:"manufacturer"`
+	PrescriptionRequired bool    `json:"prescription_required"`
+	AvgRating            float64 `gorm:"-" json:"avg_raiting"`
 }
 
 type CreateMedicineRequest struct {
-	Name                  string  `json:"name"`
-	Description           string  `json:"description"`
-	Price                 int     `json:"price"`
-	InStock              bool    `json:"instock"`
-	StockQuantity        uint    `json:"stockquantity"`
-	CategoryId           string  `json:"categoryid"`
-	SubcategoryId        string  `json:"subcategoryid"`
-	Manufacturer          string  `json:"manufacturer"`
-	PrescriptionRequired bool    `json:"prescriptionrequired"`
-	AvgRating            float64 `gorm:"-" json:"avgraiting"`
+	Name                 string  `json:"name"`
+	Description          string  `json:"description"`
+	Price                int     `json:"price"`
+	InStock              bool    `json:"in_stock"`
+	StockQuantity        uint    `json:"stock_quantity"`
+	CategoryId           uint    `json:"category_id"`
+	SubcategoryId        uint    `json:"subcategory_id"`
+	Manufacturer         string  `json:"manufacturer"`
+	PrescriptionRequired bool    `json:"prescription_required"`
+	AvgRating            float64 `gorm:"-" json:"avg_raiting"`
 }
 type UpdateMedicineRequest struct {
-	Name                  *string  `json:"name"`
-	Description           *string  `json:"description"`
-	Price                 *int     `json:"price"`
-	InStock              *bool    `json:"instock"`
-	StockQuantity        *uint    `json:"stockquantity"`
-	CategoryId           *string  `json:"categoryid"`
-	SubcategoryId        *string  `json:"subcategoryid"`
-	Manufacturer          *string  `json:"manufacturer"`
-	PrescriptionRequired *bool    `json:"prescriptionrequired"`
-	AvgRating            *float64 `gorm:"-" json:"avgraiting"`
+	Name                 *string  `json:"name"`
+	Description          *string  `json:"description"`
+	Price                *int     `json:"price"`
+	InStock              *bool    `json:"in_stock"`
+	StockQuantity        *uint    `json:"stock_quantity"`
+	CategoryId           *uint    `json:"category_id"`
+	SubcategoryId        *uint    `json:"subcategory_id"`
+	Manufacturer         *string  `json:"manufacturer"`
+	PrescriptionRequired *bool    `json:"prescription_required"`
+	AvgRating            *float64 `gorm:"-" json:"avg_raiting"`
 }
--- internal/models/review.go
+++ internal/models/review.go
@@ -5,21 +5,21 @@ import "gorm.io/gorm"
 type Review struct {
 	gorm.Model
 	UserID     string `json:"user_id"`
-	MedicineID string `json:"medicine_id"`
+	MedicineID uint   `json:"medicine_id"`
 	Rating     int    `json:"rating"`
 	Text       string `json:"text"`
 }
 
 type ReviewCreateInput struct {
 	UserID     string `json:"user_id"`
-	MedicineID string `json:"medicine_id"`
+	MedicineID uint   `json:"medicine_id"`
 	Rating     int    `json:"rating"`
 	Text       string `json:"text"`
 }
 
 type ReviewUpdateInput struct {
 	UserID     *string `json:"user_id"`
-	MedicineID *string `json:"medicine_id`
+	MedicineID *uint   `json:"medicine_id"`
 	Rating     *int    `json:"rating"`
 	Text       *string `json:"text"`
 }
